a56c8e688ee5b56e8549e718a518123daa069a02
jaydensipe <jayden.sipe@gmail.com>
2020-05-14 20:58:06 -0400

    Fixed level changer

--- FarmNer/Levels/StartingLevel.gd
+++ FarmNer/Levels/StartingLevel.gd
@@ -5,13 +5,9 @@ signal onPlayerEnterLvl3
 
 # Area Portal from Lvl 1 -> 2
 func _on_Zone1_body_entered(body):
-	var groups = body.get_groups()
-	if (groups.has("Player")):
 		emit_signal("onPlayerEnterLvl2")
 		
 # Area Portal from Lvl 1 -> 3
 func _on_Zone2_body_entered(body):
-	var groups = body.get_groups()
-	if (groups.has("Player")):
 		emit_signal("onPlayerEnterLvl3")
 
