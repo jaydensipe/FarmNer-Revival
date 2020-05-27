e0e0f90eba0c037e54b94da352612c2f64b03823
jaydensipe <jayden.sipe@gmail.com>
2020-05-26 20:38:28 -0400

    Fixed bandage orb bar

--- FarmNer/Assets/OrbKillBar/OrbKillBar.gd
+++ FarmNer/Assets/OrbKillBar/OrbKillBar.gd
@@ -1,6 +1,14 @@
 extends Node2D
 
+var canShowText = true
 
 # Sets HUD text to current number of bandages
 func _process(delta):
 	$Label.set_text(str(GLOBAL.orbKills))
+	showOrbBarUnlockText()
+
+	
+func showOrbBarUnlockText():
+	if (GLOBAL.orbDestroyerUnlocked == true && canShowText == true):
+		$AnimationPlayer.play("ShowOrbBar")
+		canShowText = false
